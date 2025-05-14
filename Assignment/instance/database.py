4b53f78efb485f35a20ca78f5c4244b2c72aef8b
wais4u <uwaismy@gmail.com>
2025-05-14 23:26:52 +0800

    trying to connect with my local sql server

--- Assignment/instance/database.py
+++ Assignment/instance/database.py
@@ -1,1 +1,16 @@
-# tesintg bruh
+# Current setup of the database USING SSMS QUERY
+
+'''
+
+CREATE DATABASE diary_app;
+GO
+
+USE diary_app;
+GO
+
+CREATE TABLE Users (
+    username NVARCHAR(50) NOT NULL UNIQUE,
+    password NVARCHAR(50) NOT NULL
+)
+
+'''
