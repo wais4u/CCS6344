7ff0e9e5daf3709b1f137f09f8e27e2308417dab
wais4u <uwaismy@gmail.com>
2025-05-14 13:56:21 +0800

    wip doing db connect

--- Assignment/test.py
+++ Assignment/test.py
@@ -1,6 +1,13 @@
+import pyodbc
 from flask import Flask
 
 app = Flask(__name__)
+
+conn = pyodbc.connect(
+    'DRIVER={testing};'
+    'SERVER=192.168.50.101;'
+    'DATABASE=EncryptedDB;'
+)
 
 @app.route("/")
 def hello_world():
